0f68e4531abcfd55cb2d3aba0585a230174056f7
marcoswlrich <marcosapw.dev@gmail.com>
2023-07-13 23:35:21 -0400

    Config Context

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"context"
 	"os"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	lambda "github.com/aws/aws-lambda-go/lambda"
 
 	"github.com/marcoswlrich/twittergo/awsgo"
+	"github.com/marcoswlrich/twittergo/models"
 	"github.com/marcoswlrich/twittergo/secretmanager"
 )
 
@@ -45,6 +47,18 @@ func ExecuteLambda(
 		}
 		return res, nil
 	}
+
+	path := strings.Replace(request.PathParameters["twittergo"], os.Getenv("UrlPrefix"), "", -1)
+
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("path"), path)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("method"), request.HTTPMethod)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("user"), SecretModel.Username)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("password"), SecretModel.Password)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("host"), SecretModel.Host)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("database"), SecretModel.Database)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("jwtSign"), SecretModel.JWTSign)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("body"), request.Body)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("bucketName"), os.Getenv("BucketName"))
 }
 
 func ValidateParameters() bool {
