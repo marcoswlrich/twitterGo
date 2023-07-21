993f5e3101028e17d41ef3a870b5fbb9175882a0
marcoswlrich <marcosapw.dev@gmail.com>
2023-07-20 23:35:33 -0400

    Config registro de usuarios

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 
+	"github.com/marcoswlrich/twittergo/jwt"
 	"github.com/marcoswlrich/twittergo/models"
 )
 
@@ -17,9 +18,18 @@ func Manipuladores(ctx context.Context, request events.APIGatewayProxyRequest) m
 	var r models.RespApi
 	r.Status = 400
 
+	isOk, statuscode, msg, claim := validaAuthorization(ctx, request)
+	if !isOk {
+		r.Status = statuscode
+		r.Message = msg
+		return r
+	}
+
 	switch ctx.Value(models.Key("method")).(string) {
 	case "POST":
 		switch ctx.Value(models.Key("path")).(string) {
+		case "registro":
+			return routers.Registro(ctx)
 		}
 
 	case "GET":
@@ -35,4 +45,33 @@ func Manipuladores(ctx context.Context, request events.APIGatewayProxyRequest) m
 
 	r.Message = "Method Invalid"
 	return r
+}
+
+func validaAuthorization(
+	ctx context.Context,
+	request events.APIGatewayProxyRequest,
+) (bool, int, string, models.Claim) {
+	path := ctx.Value(models.Key("path")).(string)
+	if path == "registro" || path == "login" || path == "obterAvatar" || path == "obterBanner" {
+		return true, 200, "", models.Claim{}
+	}
+
+	token := request.Headers["Authorization"]
+	if len(token) == 0 {
+		return false, 401, "Token obrigatorio", models.Claim{}
+	}
+
+	claim, tudoOk, msg, err := jwt.ProcessToken(token, ctx.Value(models.Key("jwtSign")).(string))
+	if !tudoOk {
+		if err != nil {
+			fmt.Println("Error token" + err.Error())
+			return false, 401, err.Error(), models.Claim{}
+		} else {
+			fmt.Println("Erro Token" + msg)
+			return false, 401, msg, models.Claim{}
+		}
+	}
+
+	fmt.Println("Token Ok")
+	return true, 200, msg, *claim
 }
